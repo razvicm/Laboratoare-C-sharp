e0b932e1c09a7690033691a7e7a30d38a77d4939
Razvan <razvan@yahoo.com>
2017-11-19 14:23:10 +0200

    Tema lab2_V1

---
+++ Lab2/Tema/Tema/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace Tema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.r3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.r4 = new System.Windows.Forms.RadioButton();
+            this.r6 = new System.Windows.Forms.RadioButton();
+            this.r5 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.r7 = new System.Windows.Forms.RadioButton();
+            this.r9 = new System.Windows.Forms.RadioButton();
+            this.r8 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.r10 = new System.Windows.Forms.RadioButton();
+            this.r12 = new System.Windows.Forms.RadioButton();
+            this.r11 = new System.Windows.Forms.RadioButton();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.r13 = new System.Windows.Forms.RadioButton();
+            this.r15 = new System.Windows.Forms.RadioButton();
+            this.r14 = new System.Windows.Forms.RadioButton();
+            this.verif = new System.Windows.Forms.Button();
+            this.inchide = new System.Windows.Forms.Button();
+            this.punctaj = new System.Windows.Forms.Label();
+            this.rezultat = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Esti: Prietenos/ Echilibrat. Detasat";
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Location = new System.Drawing.Point(20, 19);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(194, 17);
+            this.r1.TabIndex = 2;
+            this.r1.TabStop = true;
+            this.r1.Text = "a. Te gandesti: in sfarsit prieteni noi.";
+            this.r1.UseVisualStyleBackColor = true;
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Location = new System.Drawing.Point(20, 42);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(185, 17);
+            this.r2.TabIndex = 3;
+            this.r2.TabStop = true;
+            this.r2.Text = "b. Noul te sperie, insa te si atrage.";
+            this.r2.UseVisualStyleBackColor = true;
+            // 
+            // r3
+            // 
+            this.r3.AutoSize = true;
+            this.r3.Location = new System.Drawing.Point(20, 65);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(253, 17);
+            this.r3.TabIndex = 4;
+            this.r3.TabStop = true;
+            this.r3.Text = "c. Te simti mai bine decat majoritatea angajatilor.";
+            this.r3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.r1);
+            this.groupBox1.Controls.Add(this.r3);
+            this.groupBox1.Controls.Add(this.r2);
+            this.groupBox1.Location = new System.Drawing.Point(69, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 108);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Esti in prima zi la noul loc de munca. Cum te simti?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.r4);
+            this.groupBox2.Controls.Add(this.r6);
+            this.groupBox2.Controls.Add(this.r5);
+            this.groupBox2.Location = new System.Drawing.Point(69, 171);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(450, 108);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2. Vecinii sarbatoresc si sunt cam zgomotosi. Esti obosit.Cum reactionezi?";
+            // 
+            // r4
+            // 
+            this.r4.AutoSize = true;
+            this.r4.Location = new System.Drawing.Point(20, 19);
+            this.r4.Name = "r4";
+            this.r4.Size = new System.Drawing.Size(181, 17);
+            this.r4.TabIndex = 2;
+            this.r4.TabStop = true;
+            this.r4.Text = "a. Suni si le ceri iritat sa inceteze.";
+            this.r4.UseVisualStyleBackColor = true;
+            // 
+            // r6
+            // 
+            this.r6.AutoSize = true;
+            this.r6.Location = new System.Drawing.Point(20, 65);
+            this.r6.Name = "r6";
+            this.r6.Size = new System.Drawing.Size(248, 17);
+            this.r6.TabIndex = 4;
+            this.r6.TabStop = true;
+            this.r6.Text = "c. Bati la usa cu speranta ca te invita si pe tine.";
+            this.r6.UseVisualStyleBackColor = true;
+            // 
+            // r5
+            // 
+            this.r5.AutoSize = true;
+            this.r5.Location = new System.Drawing.Point(20, 42);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(160, 17);
+            this.r5.TabIndex = 3;
+            this.r5.TabStop = true;
+            this.r5.Text = "b. Folosesti dopuri de urechi.";
+            this.r5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.r7);
+            this.groupBox3.Controls.Add(this.r9);
+            this.groupBox3.Controls.Add(this.r8);
+            this.groupBox3.Location = new System.Drawing.Point(69, 285);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(450, 108);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "3. Un prieten te invita la cina. Ajuns acolo gasesti casa plina de oameni. Cum te" +
+    " comporti?";
+            // 
+            // r7
+            // 
+            this.r7.AutoSize = true;
+            this.r7.Location = new System.Drawing.Point(20, 19);
+            this.r7.Name = "r7";
+            this.r7.Size = new System.Drawing.Size(275, 17);
+            this.r7.TabIndex = 2;
+            this.r7.TabStop = true;
+            this.r7.Text = "a. Ar fi trebuit sa te anunte. Daca ai fi mers in pijama?";
+            this.r7.UseVisualStyleBackColor = true;
+            // 
+            // r9
+            // 
+            this.r9.AutoSize = true;
+            this.r9.Location = new System.Drawing.Point(20, 65);
+            this.r9.Name = "r9";
+            this.r9.Size = new System.Drawing.Size(179, 17);
+            this.r9.TabIndex = 4;
+            this.r9.TabStop = true;
+            this.r9.Text = "c. Te superi si ii arati acest lucru.";
+            this.r9.UseVisualStyleBackColor = true;
+            // 
+            // r8
+            // 
+            this.r8.AutoSize = true;
+            this.r8.Location = new System.Drawing.Point(20, 42);
+            this.r8.Name = "r8";
+            this.r8.Size = new System.Drawing.Size(224, 17);
+            this.r8.TabIndex = 3;
+            this.r8.TabStop = true;
+            this.r8.Text = "b. Mai multe persoane mai multa distractie.";
+            this.r8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.r10);
+            this.groupBox4.Controls.Add(this.r12);
+            this.groupBox4.Controls.Add(this.r11);
+            this.groupBox4.Location = new System.Drawing.Point(69, 399);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(450, 108);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "4. Completeaza aceasta fraza: „Oamenii sunt...”";
+            // 
+            // r10
+            // 
+            this.r10.AutoSize = true;
+            this.r10.Location = new System.Drawing.Point(20, 19);
+            this.r10.Name = "r10";
+            this.r10.Size = new System.Drawing.Size(77, 17);
+            this.r10.TabIndex = 2;
+            this.r10.TabStop = true;
+            this.r10.Text = "a. simpatici";
+            this.r10.UseVisualStyleBackColor = true;
+            // 
+            // r12
+            // 
+            this.r12.AutoSize = true;
+            this.r12.Location = new System.Drawing.Point(20, 65);
+            this.r12.Name = "r12";
+            this.r12.Size = new System.Drawing.Size(110, 17);
+            this.r12.TabIndex = 4;
+            this.r12.TabStop = true;
+            this.r12.Text = "c. uneori nedrepti.";
+            this.r12.UseVisualStyleBackColor = true;
+            // 
+            // r11
+            // 
+            this.r11.AutoSize = true;
+            this.r11.Location = new System.Drawing.Point(20, 42);
+            this.r11.Name = "r11";
+            this.r11.Size = new System.Drawing.Size(79, 17);
+            this.r11.TabIndex = 3;
+            this.r11.TabStop = true;
+            this.r11.Text = "b. misteriosi";
+            this.r11.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.r13);
+            this.groupBox5.Controls.Add(this.r15);
+            this.groupBox5.Controls.Add(this.r14);
+            this.groupBox5.Location = new System.Drawing.Point(69, 513);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(448, 108);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "5. Cel mai tare te emotioneaza...";
+            // 
+            // r13
+            // 
+            this.r13.AutoSize = true;
+            this.r13.Location = new System.Drawing.Point(20, 19);
+            this.r13.Name = "r13";
+            this.r13.Size = new System.Drawing.Size(123, 17);
+            this.r13.TabIndex = 2;
+            this.r13.TabStop = true;
+            this.r13.Text = "a. Un cadou surpriza";
+            this.r13.UseVisualStyleBackColor = true;
+            // 
+            // r15
+            // 
+            this.r15.AutoSize = true;
+            this.r15.Location = new System.Drawing.Point(20, 65);
+            this.r15.Name = "r15";
+            this.r15.Size = new System.Drawing.Size(182, 17);
+            this.r15.TabIndex = 4;
+            this.r15.TabStop = true;
+            this.r15.Text = "c. O provocare la locul de munca";
+            this.r15.UseVisualStyleBackColor = true;
+            // 
+            // r14
+            // 
+            this.r14.AutoSize = true;
+            this.r14.Location = new System.Drawing.Point(20, 42);
+            this.r14.Name = "r14";
+            this.r14.Size = new System.Drawing.Size(153, 17);
+            this.r14.TabIndex = 3;
+            this.r14.TabStop = true;
+            this.r14.Text = "b. O declaratie de dragoste";
+            this.r14.UseVisualStyleBackColor = true;
+            // 
+            // verif
+            // 
+            this.verif.Location = new System.Drawing.Point(74, 689);
+            this.verif.Name = "verif";
+            this.verif.Size = new System.Drawing.Size(75, 23);
+            this.verif.TabIndex = 8;
+            this.verif.Text = "Verifica";
+            this.verif.UseVisualStyleBackColor = true;
+            this.verif.Click += new System.EventHandler(this.verif_Click);
+            // 
+            // inchide
+            // 
+            this.inchide.Location = new System.Drawing.Point(351, 689);
+            this.inchide.Name = "inchide";
+            this.inchide.Size = new System.Drawing.Size(75, 23);
+            this.inchide.TabIndex = 9;
+            this.inchide.Text = "Renunta";
+            this.inchide.UseVisualStyleBackColor = true;
+            this.inchide.Click += new System.EventHandler(this.inchide_Click);
+            // 
+            // punctaj
+            // 
+            this.punctaj.AutoSize = true;
+            this.punctaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punctaj.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.punctaj.Location = new System.Drawing.Point(89, 624);
+            this.punctaj.Name = "punctaj";
+            this.punctaj.Size = new System.Drawing.Size(0, 20);
+            this.punctaj.TabIndex = 10;
+            // 
+            // rezultat
+            // 
+            this.rezultat.AutoSize = true;
+            this.rezultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rezultat.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.rezultat.Location = new System.Drawing.Point(89, 662);
+            this.rezultat.Name = "rezultat";
+            this.rezultat.Size = new System.Drawing.Size(0, 20);
+            this.rezultat.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(684, 592);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.punctaj);
+            this.Controls.Add(this.inchide);
+            this.Controls.Add(this.verif);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.RadioButton r3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton r4;
+        private System.Windows.Forms.RadioButton r6;
+        private System.Windows.Forms.RadioButton r5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton r7;
+        private System.Windows.Forms.RadioButton r9;
+        private System.Windows.Forms.RadioButton r8;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton r10;
+        private System.Windows.Forms.RadioButton r12;
+        private System.Windows.Forms.RadioButton r11;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RadioButton r13;
+        private System.Windows.Forms.RadioButton r15;
+        private System.Windows.Forms.RadioButton r14;
+        private System.Windows.Forms.Button verif;
+        private System.Windows.Forms.Button inchide;
+        private System.Windows.Forms.Label punctaj;
+        private System.Windows.Forms.Label rezultat;
+    }
+}
+
---
+++ Lab2/Tema/Tema/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema
+{
+    public partial class Form1 : Form
+    {
+        int[] total = new int[5];
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void verif_Click(object sender, EventArgs e)
+        {
+
+            if (r1.Checked)
+                total[0] = 1;
+            else if (r2.Checked)
+                total[0] = 2;
+            else if (r3.Checked)
+                total[0]= 3;
+
+            if (r4.Checked)
+                total [1]= 3;
+            else if (r5.Checked)
+            {
+                total [1]= 2;
+                Console.WriteLine(total);
+            }
+            else if (r6.Checked)
+                total [1]= 1;
+
+            if (r7.Checked)
+                total [2]= 2;
+            else if (r8.Checked)
+                total [2]= 1;
+            else if (r9.Checked)
+                total [2]= 3;
+            if (r10.Checked)
+                total [3]= 1;
+            else if (r11.Checked)
+                total [3]= 3;
+            else if (r12.Checked)
+                total [3]= 2;
+
+             if (r13.Checked)
+                total [4]= 1;
+            else if (r14.Checked)
+                total [4]= 2;
+            else if (r15.Checked)
+                total [4]= 3;
+            //punctaj.Text = ("Ai obtinut {0}",total +" puncte");
+            punctaj.Text = ("Ai obtinut "+ total.Sum() + " puncte");
+            if(total.Sum()<9)
+            rezultat.Text = " Lasi impresia unei persoane care se adapteaza foarte usor unor situatii noi.";
+            else if (total.Sum() >= 9)
+                rezultat.Text = "  La prima vedere lasi impresia unei persoane foarte echilibrate.";
+            if (total.Sum() > 11)
+                rezultat.Text = " Primul lucru care se observa la tine este siguranta de care dai dovada in actiunile tale.";
+        }
+
+        private void inchide_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
